84943a584368ad5532a886b6063ce4fd4f380bae
jawad_alkatreeb <jawadalkatreeb@gmail.com>
2026-06-25 17:30:25 +0300

    "Implement JWT Authentication, Authorization middleware, and secure transaction endpoints"

--- BankApi/Services/ICustomerServices.cs
+++ BankApi/Services/ICustomerServices.cs
@@ -9,12 +9,10 @@ namespace BankApi.Services
         Task<IEnumerable<CustomerResponseDto>> GetAllCustomersAsync(string? searchTerm = null);
         Task<CustomerResponseDto> GetCustomerByIdAsync(int id);
         Task<CustomerResponseDto> RegisterCustomerAsync(RegisterCustomerDto dto);
-        Task<CustomerResponseDto> LoginAsync(LoginDto dto);
+        Task<LoginResponseDto> LoginAsync(LoginDto dto);
         Task<bool> UpdateCustomerAsync(int id, UpdateCustomerDto dto);
         Task<bool> ChangePasswordAsync(int id, ChangePasswordDto dto);
 
         Task<bool> ToggleCustomerStatusAsync(int id, bool isActive);
-        //bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt);
-        //void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt);
     }
 }
